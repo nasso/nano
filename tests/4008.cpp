1b4c06d58367399ece6702aced84368b851d76ea
nasso <nassomails@gmail.com>
2021-02-14 05:39:51 +0100

    Add 4008

--- tests/4008.cpp
+++ tests/4008.cpp
@@ -5,9 +5,12 @@
 ** nts_components
 */
 
+#include "InputComponent.hpp"
 #include "NTSCircuit.hpp"
+#include "OutputComponent.hpp"
 #include "assert_truth.hpp"
 #include <criterion/criterion.h>
+#include <criterion/theories.h>
 
 const auto T = nts::Tristate::TRUE;
 const auto F = nts::Tristate::FALSE;
@@ -76,4 +79,88 @@ Test(components_4008, adder_tristate)
             << std::flush;
         criterion::logging::info << gate << std::flush;
     }
+}
+
+TheoryDataPoints(components_4008, binary_4008) = {
+    DataPoints(nts::Tristate, T, F),
+    DataPoints(nts::Tristate, T, F),
+    DataPoints(nts::Tristate, T, F),
+    DataPoints(nts::Tristate, T, F),
+    DataPoints(nts::Tristate, T, F),
+    DataPoints(nts::Tristate, T, F),
+    DataPoints(nts::Tristate, T, F),
+    DataPoints(nts::Tristate, T, F),
+    DataPoints(nts::Tristate, T, F),
+};
+
+Theory(
+    (
+        nts::Tristate c1,
+        nts::Tristate a1,
+        nts::Tristate a2,
+        nts::Tristate a3,
+        nts::Tristate a4,
+        nts::Tristate b1,
+        nts::Tristate b2,
+        nts::Tristate b3,
+        nts::Tristate b4),
+    components_4008,
+    binary_4008,
+    .timeout = 5)
+{
+    nts::NTSCircuit gate("components/4008.nts");
+    nts::InputComponent inputs[] = {
+        c1,
+        a1,
+        a2,
+        a3,
+        a4,
+        b1,
+        b2,
+        b3,
+        b4,
+    };
+    nts::OutputComponent s1, s2, s3, s4, co;
+
+    // initialize inputs
+    for (auto& in : inputs) {
+        in.simulate(1);
+    }
+
+    gate.setLink(9, inputs[0], 1);
+    gate.setLink(7, inputs[1], 1);
+    gate.setLink(5, inputs[2], 1);
+    gate.setLink(3, inputs[3], 1);
+    gate.setLink(1, inputs[4], 1);
+    gate.setLink(6, inputs[5], 1);
+    gate.setLink(4, inputs[6], 1);
+    gate.setLink(2, inputs[7], 1);
+    gate.setLink(15, inputs[8], 1);
+    gate.setLink(10, s1, 1);
+    gate.setLink(11, s2, 1);
+    gate.setLink(12, s3, 1);
+    gate.setLink(13, s4, 1);
+    gate.setLink(14, co, 1);
+
+    gate.simulate(1);
+
+    std::uint8_t c = c1 == T;
+    std::uint8_t a = 0
+        | ((a1 == T) << 0)
+        | ((a2 == T) << 1)
+        | ((a3 == T) << 2)
+        | ((a4 == T) << 3);
+    std::uint8_t b = 0
+        | ((b1 == T) << 0)
+        | ((b2 == T) << 1)
+        | ((b3 == T) << 2)
+        | ((b4 == T) << 3);
+    std::uint8_t s = 0
+        | ((s1 == T) << 0)
+        | ((s2 == T) << 1)
+        | ((s3 == T) << 2)
+        | ((s4 == T) << 3)
+        | ((co == T) << 4);
+
+    cr_assert_eq(a + b + c, s);
 }
