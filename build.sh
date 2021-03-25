e4bb345681a39568b34db2ef6a8c27d1f2f193a4
nasso <nassomails@gmail.com>
2021-03-25 20:01:21 +0100

    Fix owner for files built in emsdk docker

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ echo "<-------------------------->"
 (
   APP_DIR=$(pwd)
   BUILD_DIR=/tmp/libnts/build
+  OWNER=$(stat -c "%u:%g" $APP_DIR)
 
   rm -rf $BUILD_DIR
   mkdir -p $BUILD_DIR
@@ -24,6 +25,10 @@ echo "<-------------------------->"
   mkdir -p "$APP_DIR/public/build"
   cp -f ./nts.wasm "$APP_DIR/public/build/nts.wasm"
   cp -f ./nts.js "$APP_DIR/src/nts.js"
+
+  chown $OWNER "$APP_DIR/public/build"
+  chown $OWNER "$APP_DIR/public/build/nts.wasm"
+  chown $OWNER "$APP_DIR/src/nts.js"
 )
 echo "<------------------------->"
 echo "<-- Built successfully! -->"
